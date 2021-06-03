848d7abc7592252e7016ced12e0ee031f4d9529f
Navid Rojiani <nrojiani@gmail.com>
2021-06-02 22:21:35 -0400

    Setup Dependency Injection with Hilt
    Description & Context
    =====================
    
    Related Links
    -------------
    https://developer.android.com/training/dependency-injection/hilt-android
    
    Testing Done
    ------------

--- app/src/main/java/com/nrojiani/bartender/viewmodels/CocktailViewModel.kt
+++ app/src/main/java/com/nrojiani/bartender/viewmodels/CocktailViewModel.kt
@@ -2,15 +2,25 @@ package com.nrojiani.bartender.viewmodels
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
+import com.nrojiani.bartender.utils.viewmodel.navArgs
+import com.nrojiani.bartender.views.CocktailFragmentArgs
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
 
-class CocktailViewModel(cocktailName: String) : ViewModel() {
+@HiltViewModel
+class CocktailViewModel @Inject constructor(
+    savedStateHandle: SavedStateHandle,
+) : ViewModel() {
 
-    private var _selectedCocktailName = MutableLiveData<String>()
+    /**
+     * Workaround for Hilt not playing nicely with SafeArgs.
+     * @see [navArgs]
+     */
+    private val fragmentArgs: CocktailFragmentArgs by savedStateHandle.navArgs()
+
+    private var _selectedCocktailName = MutableLiveData(fragmentArgs.cocktailName)
     val selectedCocktailName: LiveData<String>
         get() = _selectedCocktailName
-
-    init {
-        _selectedCocktailName.value = cocktailName
-    }
 }
