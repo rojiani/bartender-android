c49ca710f645d6ea5746af84d67d0ef831364982
Navid Rojiani <nrojiani@gmail.com>
2021-06-13 13:05:29 -0400

    NetworkStatusMonitor - flow for connectivity changes
    Description & Context
    =====================
    Display message on SearchFragment when offline
    
    Related Links
    -------------
    
    Testing Done
    ------------

---
+++ app/src/main/java/com/nrojiani/bartender/utils/connectivity/NetworkStatus.kt
@@ -0,0 +1,11 @@
+package com.nrojiani.bartender.utils.connectivity
+
+enum class NetworkStatus {
+    CONNECTED,
+    NOT_CONNECTED,
+
+    /**
+     * Initial value if not yet checked.
+     */
+    UNDETERMINED
+}
